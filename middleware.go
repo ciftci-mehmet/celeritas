294db64b791cbb09be77763ca7f13c2615fc7a01
Mehmet Çiftçi <mciftci@maritimetrainer.com>
2023-04-13 15:52:41 +0300

    Section 14: Redis sessions, csrf, debug mode

--- middleware.go
+++ middleware.go
@@ -1,7 +1,30 @@
 package celeritas
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/justinas/nosurf"
+)
 
 func (c *Celeritas) SessionLoad(next http.Handler) http.Handler {
 	return c.Session.LoadAndSave(next)
+}
+
+func (c *Celeritas) NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	secure, _ := strconv.ParseBool(c.config.cookie.secure)
+
+	// ignore csrftokens coming from following pattern
+	csrfHandler.ExemptGlob("/api/*")
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		Path:     "/",
+		Domain:   c.config.cookie.domain,
+		Secure:   secure,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	return csrfHandler
 }
